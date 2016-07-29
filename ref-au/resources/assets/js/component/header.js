ede1ae1810d21d4b5a7122f18158fc7072db67ab
David Andika <david_andika17123@hotmail.com>
2016-07-29 10:49:33 +1000

    Update site header to be fixed on top of page

--- ref-au/resources/assets/js/component/header.js
+++ ref-au/resources/assets/js/component/header.js
@@ -1,5 +1,24 @@
 (function header($) {
     $(document).ready(function () {
+        /**
+         * Updates the style of header based on scroll position
+         */
+        var updateStyle = function () {
+            var $header = $('.js-site-header');
+            var scrollTop = $('body').scrollTop();
+            var headerHeight = $header.height();
+
+            if (scrollTop >= headerHeight)
+            {
+                $header.addClass('site-header--opaque');
+            }
+            else
+            {
+                $header.removeClass('site-header--opaque');
+            }
+        };
+
+        // Universities header link
         $('.js-site-header-link').click(function (e) {
             var targetURL = $(this).attr('href');
             var urlParts = $.parseURL(targetURL);
@@ -25,7 +44,14 @@
                     e.preventDefault();
                 }
             }
-
         });
+
+        // Header on scroll
+        $(document).on('scroll', function () {
+            updateStyle();
+        });
+
+        // Init
+        updateStyle();
     });
 })(jQuery);
