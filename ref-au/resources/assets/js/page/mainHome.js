7ef1148bd4adc52e352f70f4529340fdf1dc64f3
David Andika <david_andika17123@hotmail.com>
2017-07-28 09:18:44 +1000

    Modified behavior of uni slices on main home page

--- ref-au/resources/assets/js/page/mainHome.js
+++ ref-au/resources/assets/js/page/mainHome.js
@@ -3,7 +3,7 @@
      * @param string uniSlice The class name that points to the uni slice
      */
     var openUniSlice = function (uniSlice) {
-        $('.slice--university .slice-cover').removeClass('slice-cover--opened');
+        // $('.slice--university .slice-cover').removeClass('slice-cover--opened');
         $('.'+uniSlice+' .slice-cover').addClass('slice-cover--opened');
     };
 
@@ -20,8 +20,15 @@
             arrows: false,
             dots: true,
             autoplay: true,
-            autoplaySpeed: 3000
+            autoplaySpeed: 5000,
+            pauseOnHover: false
         });
+        // Make each carousel start playing at different times
+        $('.js-photos-carousel').slick('slickPause');
+        setTimeout(function () { $('.js-photos-carousel:nth(0)').slick('slickPlay'); }, 1000);
+        setTimeout(function () { $('.js-photos-carousel:nth(1)').slick('slickPlay'); }, 250);
+        setTimeout(function () { $('.js-photos-carousel:nth(2)').slick('slickPlay'); }, 1500);
+
 
         $('.js-slice-cover-toggle').click(function (e) {
             e.preventDefault();
@@ -36,5 +43,15 @@
                 openUniSlice(target);
             }
         });
+
+        $(document).on('scroll.uniSlice', $.debounce(function () {
+            var scrollTop = $('body').scrollTop() || $('html').scrollTop();
+            var threshold = $('#universities').position().top - $('.js-site-header').height();
+            if (scrollTop >= threshold) {
+                $('.slice-cover').addClass('slice-cover--opened');
+                $(document).off('scroll.uniSlice');
+            }
+        }, 300));
+        $(document).trigger('scroll.uniSlice');
     });
 })(jQuery);
